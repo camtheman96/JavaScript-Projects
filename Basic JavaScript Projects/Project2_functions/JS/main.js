d9d1e43c142659ce53f660295a6f9b234eb3653e
Cam <camerontietze@gmail.com>
2021-01-24 00:08:44 -0500

    Update main.js comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
 function myFunction(){ //Create function called "myFunction"
-    var sentence= "Look at all the"; //
-    sentence += " cows on this farm.";
-    document.getElementById("Concatenate").innerHTML = sentence;
+    var sentence= "Look at all the"; //Create variable called "sentence" and assign it a string value
+    sentence += " cows on this farm."; //Use += expression to add string value to "sentence" variable
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the value of "semtence" into HTML elements with "Concatenate" id
 }
